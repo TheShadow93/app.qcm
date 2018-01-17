f945ea4f775aab9f55269bc1e3a6479bd058c2c3
Waseem <waseem11@hotmail.fr>
2018-01-17 17:28:43 +0100

    aa

--- src/Project.java
+++ src/Project.java
@@ -1,14 +1,33 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
-import java.io.File;
-public class Project {
+public class Project {  
 
-		
-		
-			
-	
-	public static void main(String[] args) {
-		File file = new File("test.txt");
-		
-	}
+public static void main(String[] arguments) {
+FileReader monFichier = null;
+BufferedReader Read = null;
+try {
+monFichier = new FileReader("azerty.txt");
+Read = new BufferedReader(monFichier);
+
+while (true) {
+// Lit une ligne de test.csv
+String ligne = Read.readLine();
+// Vérifie la fin de fichier
+if (ligne == null)
+break;
+System.out.println(ligne);
+} // Fin du while
+} catch (IOException exception) {
+exception.printStackTrace();
+} finally {
+try {
+Read.close();
+monFichier.close();
+} catch(IOException exception1) {
+exception1.printStackTrace();
+			}
+		}
+	} 
 }
-
